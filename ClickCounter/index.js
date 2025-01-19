2678c6ff523bf2cbab9fca0ada4c5e0100dd3856
Jefferson Luiz Seno <jeffersonseno@gmail.com>
2025-01-19 14:16:27 -0300

    Contador de cliques melhorado

--- ClickCounter/index.js
+++ ClickCounter/index.js
@@ -1,14 +1,24 @@
-const cliqueBtn = document.getElementById("btn-click");
-const contadorBtn = document.getElementById("btn-counter");
+const buttonClick = document.getElementById("btn-click");
+const contadorParagrafo = document.getElementById("contador-paragrafo");
 let contador = 0;
 
-function addCount() {
-  contadorBtn.innerHTML = contador;
+function addContador() {
+  contadorParagrafo.innerHTML = contador;
 }
 
-cliqueBtn.addEventListener("click", () => {
+buttonClick.addEventListener("click", () => {
   contador++;
-  addCount();
+  addContador();
+
+  if (contador === 10) {
+    document.body.style.backgroundColor = "rgb(131, 115, 45)";
+  } else if (contador === 20) {
+    document.body.style.backgroundColor = "rgb(191, 202, 39)";
+  } else if (contador === 30) {
+    document.body.style.backgroundColor = "rgb(23, 177, 44)";
+  } else if (contador === 40) {
+    document.body.style.backgroundColor = "rgb(185, 21, 21)";
+  }
 });
 
-addCount();
+addContador();
