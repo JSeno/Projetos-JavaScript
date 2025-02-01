250221448ef21c4f0254359f891d6a52f78c7a92
JSeno <jeffersonseno@gmail.com>
2025-02-01 17:49:37 -0300

    Criado funções de criar produto e ler produto

--- ControleEstoque/controleEstoque.js
+++ ControleEstoque/controleEstoque.js
@@ -1,3 +1,10 @@
+// Pegando elementos pelo ID:
+const itemList = document.getElementById("itemList");
+const itemName = document.getElementById("itemName");
+const itemQuantity = document.getElementById("itemQuantity");
+const itemPrice = document.getElementById("itemPrice");
+const produtosList = document.getElementById("produtosList");
+
 // Array para salvar os produtos.
 let produtos = [];
 
@@ -17,5 +24,37 @@ function mostrarProduto() {
 
 // Função para deletar um item (Delete)
 
-adicionarProduto();
-mostrarProduto();
+// Função para Mostrar na tela os produtos.
+function renderProdutos() {
+  produtosList.innerHTML = "";
+  produtos.forEach((produto) => {
+    const li = document.createElement("li");
+    li.textContent = `${produto.nome} - ${produto.quantidade} - ${produto.preco}`;
+    produtosList.appendChild(li);
+    console.log("Novo Produto Adicionado!");
+  });
+}
+
+// Adicionar itens com o botão submit.
+itemForm.addEventListener("submit", (event) => {
+  console.log("Cliquei no botão!");
+  // Evitar recarregamento da página.
+  event.preventDefault();
+
+  // Variável para salvar os produtos quando clica no botão
+  const novoProduto = {
+    nome: itemName.value,
+    quantidade: itemQuantity.value,
+    preco: parseFloat(itemPrice.value),
+  };
+
+  adicionarProduto(novoProduto);
+  renderProdutos();
+});
+
+// Um laço para mostrar os produtos do array só para ver o que está acontecendo.
+for (let i of produtos) {
+  console.log("Mostrando Produto: ", i);
+}
+
+renderProdutos();
